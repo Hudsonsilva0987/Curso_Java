06e3c1a78114c81b6e62da01790ff4e1646cf94e
Hudsonsilva0987 <hudsonsilva0987@gmail.com>
2022-02-04 13:25:10 -0300

    Exercicio com Matrizes

---
+++ Modulo POO/src/main/java/com/hudson/silva/cursoJava/POO/Exercicios/Ex6/Matrizes.java
@@ -0,0 +1,49 @@
+package com.hudson.silva.cursoJava.POO.Exercicios.Ex6;
+
+import java.util.ArrayList;
+
+import java.util.Scanner;
+
+public class Matrizes {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tamanho = sc.nextInt();
+        int[][] mat = new int[tamanho][tamanho];
+
+        for (int i = 0; i < tamanho; i++) {
+            for (int j = 0; j < tamanho; j++) {
+                mat[i][j] = sc.nextInt();
+            }
+        }
+        diagonal(mat, tamanho);
+        negativos(mat, tamanho);
+
+
+    }
+
+    public static void diagonal(int[][] mat, int tamanho) {
+        ArrayList<Integer> diagonalP = new ArrayList<>();
+        for (int i = 0; i < tamanho; i++) {
+            for (int j = 0; j < tamanho; j++) {
+                if (i == j) {
+                    diagonalP.add(mat[i][j]);
+                }
+            }
+        }
+        for (Integer matr : diagonalP) {
+            System.out.println(matr);
+        }
+    }
+
+    public static void negativos(int[][] mat, int tamanho) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < tamanho; i++) {
+            for (int j = 0; j < tamanho; j++) {
+                if (mat[i][j] < 0) {
+                    System.out.println("Numeros negativos : " + mat[i][j]);
+                }
+            }
+        }
+    }
+}
