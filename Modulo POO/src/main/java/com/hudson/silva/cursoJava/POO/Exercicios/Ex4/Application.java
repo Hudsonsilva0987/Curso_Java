41c1e9e5a39f5d5384107e0625d493a029d2f7af
Hudsonsilva0987 <hudsonsilva0987@gmail.com>
2022-02-04 12:25:07 -0300

    Exercicio Listas

---
+++ Modulo POO/src/main/java/com/hudson/silva/cursoJava/POO/Exercicios/Ex4/Application.java
@@ -0,0 +1,41 @@
+package com.hudson.silva.cursoJava.POO.Exercicios.Ex4;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Application {
+
+    public static void main(String [] args){
+
+        String name;
+        int number;
+        Double balance;
+        String answer;
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        Account acc1 = new Account();
+
+        System.out.println("Enter Account number : ");
+        number = sc.nextInt();
+        System.out.println("Enter Account Holder : ");
+        name = sc.next();
+        System.out.println(" Is there na initial deposit ?  (y/n) ");
+        answer = sc.next();
+
+        if (answer.equalsIgnoreCase("y")){
+            System.out.println("Enter initial deposit value : ");
+            balance = sc.nextDouble();
+            acc1 = new Account(name, number, balance);
+        }else{
+             acc1 = new Account(name, number, 0.00);
+        }
+        System.out.println(acc1.toString());
+
+
+        acc1.deposit(200.00);
+        System.out.println(acc1.toString());
+        acc1.withdraw(300.00);
+        System.out.println(acc1.toString());
+
+    }
+}
---
+++ Modulo POO/src/main/java/com/hudson/silva/cursoJava/POO/Exercicios/Ex5/Application.java
@@ -0,0 +1,74 @@
+package com.hudson.silva.cursoJava.POO.Exercicios.Ex5;
+
+import java.util.*;
+
+public class Application {
+
+    public static void main ( String []args) {
+        String name;
+        int id;
+        Double salary;
+
+        Locale.setDefault(Locale.US);
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("How many employees do you want to register ? ");
+        int qnt = sc.nextInt();
+
+        List<Empregados> lista = new ArrayList<>();
+
+        for (int i = 0 ; i < qnt; i++) {
+            System.out.println("Name : ");
+            name = sc.next();
+            System.out.println("ID : ");
+            id = sc.nextInt();
+            while(hasId(lista,id)){
+                System.out.println("Id already exist try again !!");
+                id = sc.nextInt();
+            }
+            System.out.println("Salary : ");
+            salary = sc.nextDouble();
+            lista.add(new Empregados(name, id, salary));
+        }
+
+        for (Empregados list :lista) {
+            System.out.println("List of Employees : \n ");
+            System.out.println(list);
+        }
+        System.out.println("Enter the employeer id that will hava salary increase : ");
+        id = sc.nextInt();
+        while(hasPosition(lista, id) == null){
+            System.out.println("ID no exist Try again !!!");
+            id = sc.nextInt();
+        }
+        Integer pos = hasPosition(lista,  id);
+        System.out.println("Porcentage increase in salary");
+        Double porcent = sc.nextDouble();
+        lista.get(pos).upSalary(porcent);
+
+
+        for (Empregados list :lista) {
+            System.out.println("List of Employees : \n ");
+            System.out.println(list);
+        }
+
+        sc.close();
+    }
+
+
+
+    static Integer hasPosition(List<Empregados> list, int id){
+        for(int i = 0; i < list.size(); i ++){
+            if(list.get(i).getId() == id){
+                return i;
+            }
+
+        }
+        return null;
+    }
+    static Boolean hasId(List<Empregados> list, int id){
+        Empregados emp = list.stream().filter(x -> x.getId() == id ).findFirst().orElse(null);
+        return emp != null;
+    }
+}
